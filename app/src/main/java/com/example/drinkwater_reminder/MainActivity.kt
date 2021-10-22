29d7350bfd514026f02ac0e445fdd93f024900b8
Jonathan Melo <jonathanmelo04@gmail.com>
2021-10-21 21:49:40 -0300

    Definition of components in the main class

--- app/src/main/java/com/example/drinkwater_reminder/MainActivity.kt
+++ app/src/main/java/com/example/drinkwater_reminder/MainActivity.kt
@@ -2,12 +2,41 @@ package com.example.drinkwater_reminder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var edit_peso: EditText
+    private lateinit var edit_idade: EditText
+    private lateinit var bt_calcular: Button
+    private lateinit var txt_resultado_ml: TextView
+    private lateinit var ic_redefinir_dados: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportActionBar!!.hide()
+        IniciarComponentes()
+
+        bt_calcular.setOnClickListener {
+            if (edit_peso.toString().isEmpty()) {
+                Toast.makeText(this, R.string.toast_informe_peso, Toast.LENGTH_SHORT).show()
+            } else if (edit_idade.toString().isEmpty()) {
+                Toast.makeText(this, R.string.toast_informe_idade, Toast.LENGTH_SHORT).show()
+            } else {
+                val peso = edit_peso.text.toString().toDouble()
+                val idade = edit_idade.text.toString().toInt()
+            }
+        }
+
+    }
+
+    private fun IniciarComponentes() {
+        edit_peso = findViewById(R.id.edit_peso)
+        edit_idade = findViewById(R.id.edit_idade)
+        bt_calcular = findViewById(R.id.bt_calcular)
+        txt_resultado_ml = findViewById(R.id.txt_resultado_ml)
+        ic_redefinir_dados = findViewById(R.id.ic_redefinir)
     }
 }
