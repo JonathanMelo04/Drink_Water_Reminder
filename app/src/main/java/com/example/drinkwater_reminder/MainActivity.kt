71c1e4ab9aee3d81c95f7ffb15e483b99a876f13
Jonathan Melo <jonathanmelo04@gmail.com>
2021-10-22 00:32:12 -0300

    added action on reminder button and date picker

--- app/src/main/java/com/example/drinkwater_reminder/MainActivity.kt
+++ app/src/main/java/com/example/drinkwater_reminder/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.drinkwater_reminder
 
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
@@ -16,8 +17,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var txt_resultado_ml: TextView
     private lateinit var ic_redefinir_dados: ImageView
 
+    private lateinit var bt_lembrete: Button
+    private lateinit var bt_alarme: Button
+    private lateinit var txt_hora: TextView
+    private lateinit var txt_minutos: TextView
+
     private lateinit var calcularIngestaoDiaria: CalcularIngestaoDiaria
     private var resultadoML = 0.0
+
+    lateinit var timePickerDialog: TimePickerDialog
+    lateinit var calendario: Calendar
+    var horaAtual = 0
+    var minutosAtuais = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,6 +69,17 @@ class MainActivity : AppCompatActivity() {
             val dialog = alertDialog.create()
             dialog.show()
         }
+
+        bt_lembrete.setOnClickListener {
+            calendario = Calendar.getInstance()
+            horaAtual = calendario.get(Calendar.HOUR_OF_DAY)
+            minutosAtuais = calendario.get(Calendar.MINUTE)
+            timePickerDialog = TimePickerDialog(this, { timePicker: TimePicker, hourOfDay: Int, minutes: Int ->
+                    txt_hora.text = String.format("%02d",hourOfDay)
+                    txt_minutos.text = String.format("%02d",minutes)
+                },horaAtual,minutosAtuais, true)
+            timePickerDialog.show()
+        }
     }
 
     private fun IniciarComponentes() {
@@ -66,5 +88,10 @@ class MainActivity : AppCompatActivity() {
         bt_calcular = findViewById(R.id.bt_calcular)
         txt_resultado_ml = findViewById(R.id.txt_resultado_ml)
         ic_redefinir_dados = findViewById(R.id.ic_redefinir)
+
+        bt_lembrete = findViewById(R.id.bt_definir_lembrete)
+        bt_alarme = findViewById(R.id.bt_definir_alarme)
+        txt_hora = findViewById(R.id.txt_hora)
+        txt_minutos = findViewById(R.id.txt_minuto)
     }
 }
