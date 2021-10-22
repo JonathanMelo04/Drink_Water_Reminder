75adfc3e28f87b67cc3a315eb43ffe5fe8d280ed
Jonathan Melo <jonathanmelo04@gmail.com>
2021-10-21 22:12:49 -0300

    Class created to calculate the results obtained in terms of weight and age

---
+++ app/src/main/java/com/example/drinkwater_reminder/model/CalcularIngestaoDiaria.kt
@@ -0,0 +1,17 @@
+package com.example.drinkwater_reminder.model
+
+class CalcularIngestaoDiaria {
+
+    private val Ml_JOVEM = 40
+    private val Ml_ADULTO = 35
+    private val Ml_IDOSO = 30
+    private val Ml_MAIS_DE_66_ANOS = 25
+
+    private val resultdo_ML = 0
+    private val resultdo_total_ML = 0
+
+    fun CalcularTotalML(peso: Double, idade: Int){
+
+    }
+
+}
